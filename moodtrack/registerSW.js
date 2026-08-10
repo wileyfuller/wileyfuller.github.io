04b2c856179b1566759782b4725a9af637583ba8
Wiley <wiley@alltheducks.com>
2026-08-10 21:54:44 +1000

    Updated base path

--- moodtrack/registerSW.js
+++ moodtrack/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/moodtrack/sw.js', { scope: '/moodtrack/' })})}
